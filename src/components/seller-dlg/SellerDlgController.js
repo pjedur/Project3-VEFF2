24fa34944bfceef4164cabd3e08466c0d6b5765c
petur215 <psigurdarson@gmail.com>
2016-03-11 16:43:00 +0000

    sellerDlg

--- src/components/seller-dlg/SellerDlgController.js
+++ src/components/seller-dlg/SellerDlgController.js
@@ -1,1 +1,23 @@
 // todo
+angular.module("project3App").controller("SellerDlgController", {
+  function SellerDlgController($scope) {
+      $scope.seller = {
+          name: "Herbert Guðmundsson",
+          category: "Bækur",
+          imagePath: ""
+      };
+
+      $scope.onOk = function onOk() {
+
+          if ($scope.seller.name.length === 0) {
+
+              return;
+          }
+          $scope.$close($scope.seller);
+      };
+
+      $scope.onCancel = function onCancel() {
+          $scope.$dismiss();
+      }
+    }
+});
