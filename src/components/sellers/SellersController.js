fbaedcd7cfdf4133a175f0ddeb389f5ebbbb5b57
pjedur <pjedur@gmail.com>
2016-03-11 15:28:54 +0000

    Started on SellersController

--- src/components/sellers/SellersController.js
+++ src/components/sellers/SellersController.js
@@ -2,7 +2,16 @@
 
 angular.module("project3App").controller("SellersController",
 function SellersController($scope, AppResource) {
+
 	// TODO: load data from AppResource! Also, add other methods, such as to
 	// add/update sellers etc.
+
+
+	AppResource.getSellers().success(function(sellers) {
+		$scope.sellers = sellers;
+	}).error(function() {
+		$scope.errorMessage = "Could not get sellers";
+	});
+
 
 });
