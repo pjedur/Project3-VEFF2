cc722887fb0ad159a4c13d042c15bf221c6e3022
pjedur <pjedur@gmail.com>
2016-03-11 23:25:57 +0000

    Modal window now working, needs bootstrap

--- src/components/seller-dlg/SellerDlgController.js
+++ src/components/seller-dlg/SellerDlgController.js
@@ -1,10 +1,14 @@
 "use strict";
 
 angular.module("project3App").controller("SellerDlgController",
-    function SellerDlgController($scope, seller) {
-        $scope.seller = seller;
+    function SellerDlgController($scope) {
+        $scope.seller = {};
+        /*
+        $scope.seller.name = "";
+        $scope.seller.category = "";
+        $scope.imagePath = ""; */
 
-        $scope.onOk = function onOk(seller) {
+        $scope.onOk = function onOk() {
 
             if ($scope.seller.name.length === 0) {
 
--- src/components/sellers/SellersController.js
+++ src/components/sellers/SellersController.js
@@ -1,17 +1,32 @@
 "use strict";
 
 angular.module("project3App").controller("SellersController",
-function SellersController($scope, AppResource, $location) {
+function SellersController($scope, AppResource, $location, SellerDlg) {
 
 	// TODO: load data from AppResource! Also, add other methods, such as to
 	// add/update sellers etc.
 	$scope.user = "";
+
 
 	AppResource.getSellers().success(function(sellers) {
 		$scope.sellers = sellers;
 	}).error(function() {
 		$scope.errorMessage = "Could not get sellers";
 	});
+
+
+
+	$scope.addSeller = function addSeller() {
+		SellerDlg.show().then(function(seller) {
+			AppResource.addSeller(seller).success(function(data) {
+					$scope.newSeller = data;
+					console.log("new seller : " + data);
+			}).error(function() {
+				$scope.errorMessage = "Could not add seller";
+				// centris notify
+			});
+		});
+	}
 
 
 });
