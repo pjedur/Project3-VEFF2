af2322092bf4aefe44b1910ce7ae96254bcee2f4
pjedur <pjedur@gmail.com>
2016-03-11 15:07:59 +0000

    little push for pelvar

--- src/app.js
+++ src/app.js
@@ -5,5 +5,8 @@ angular.module("project3App", ["ngRoute", "ui.bootstrap", "sharedServices"])
 	$routeProvider.when("/", {
 		controller: "SellersController",
 		templateUrl: "components/sellers/index.html"
+	}).when("/seller/:sellerID", {
+		controller: "SellerController",
+		templateUrl: "components/seller-details/index.html"
 	});
 });
