2138fd25353462199de7955a7e2946e32fe925cb
pjedur <pjedur@gmail.com>
2016-03-13 23:01:45 +0000

    Little progress with unit testing on SellersController

--- src/components/sellers/SellersController.spec.js
+++ src/components/sellers/SellersController.spec.js
@@ -1,21 +1,69 @@
 "use strict";
 
-describe("SellersController should be unit tested here", function() {
-	// TODO: add beforeEach/describe/it/etc. functions as appropriate!
+describe("SellersController", function() {
+
+	var successObj = {
+		name: "bla",
+		category: "lol",
+		imagePath: "lala"
+	};
+
+	var errorObj = {
+		name: "",
+		category: "",
+		imagePath: ""
+	};
 
 	beforeEach(module("project3App"));
-	var SellersController, scope;
+	var SellersController, scope, appResource, centrisnotify, sellerDlg;
 
-	beforeEach(inject(function($rootScope, $controller) {
-		scope = $rootScope.new();
+	beforeEach(inject(function($rootScope, $controller, AppResource, centrisNotify, SellerDlg) {
+		scope = $rootScope.$new();
+		centrisnotify = centrisNotify;
+		appResource = AppResource;
+		sellerDlg = SellerDlg;
+
 		SellersController = $controller("SellersController", {
-			$scope : scope
+			$scope        : scope,
+			centrisNotify : centrisnotify,
+			AppResource   : appResource,
+			SellerDlg     : sellerDlg
 		});
+
+	//	spyOn(centrisnotify, "error");
+	//	spyOn(centrisnotify, "success");
+		spyOn(sellerDlg, "show")
+		spyOn(scope, "addSeller");
+		spyOn(scope, "editSeller");
+		spyOn(appResource, "addSeller");
 	}));
 
-	xit("should get all sellers", function() {
+	describe("addseller", function() {
+		beforeEach(function() {
+			scope.addSeller(successObj);
+		});
+
+		it("should get all sellers", function() {
+			expect(scope.sellers).toBeDefined();
+			expect(scope.sellers.length).toBeGreaterThan(0);
+			expect(scope.addSeller).toHaveBeenCalledWith(successObj);
+			//expect(sellerDlg.show).toHaveBeenCalled();
+		//	expect(centrisnotify.success).toHaveBeenCalled();
+		});
+
 
 	});
+
+	describe("editSeller", function() {
+		beforeEach(function() {
+			scope.editSeller(successObj);
+		});
+
+		it("should blanla", function() {
+			expect(scope.editSeller).toHaveBeenCalledWith(successObj);
+		});
+	});
+
 
 	xit("should set centrisnotify if error getting sellers", function() {
 
